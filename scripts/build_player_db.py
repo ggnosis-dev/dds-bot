2b461ff6b124bd3480b1abd72b3c3d8849687ea6
ggnosis-dev <ggnosis.var@pm.me>
2026-04-27 14:45:39 +1000

    Updating database for git

--- scripts/build_player_db.py
+++ scripts/build_player_db.py
@@ -10,9 +10,10 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 	cursor = conn.cursor()
 
 	# TESTS:
-	cursor.execute('DROP TABLE IF EXISTS players')
+	# cursor.execute('DROP TABLE IF EXISTS players')
 	# cursor.execute('DROP TABLE IF EXISTS player_demons')
 	# cursor.execute('DROP TABLE IF EXISTS player_gems')
+	# cursor.execute('DROP TABLE IF EXISTS player_items')
 	# cursor.execute('UPDATE player_demons SET in_party = 0')
 
 	cursor.execute('''
