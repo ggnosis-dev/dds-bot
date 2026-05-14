2f6b57df156e0595bd4ebf2d6fb197dcc812a265
ggnosis-dev <ggnosis.var@pm.me>
2026-05-14 16:36:10 +1000

    Fixed scripts for formatter

--- scripts/build_player_db.py
+++ scripts/build_player_db.py
@@ -2,7 +2,6 @@ import sqlite3
 
 from database_paths import PLAYERS_DB_PATH, ensure_db_dir_exists
 
-
 ensure_db_dir_exists()
 
 # https://www.sqlitetutorial.net/sqlite-json/
@@ -16,7 +15,7 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 	# cursor.execute('DROP TABLE IF EXISTS player_gems')
 	# cursor.execute('DROP TABLE IF EXISTS player_items')
 
-	cursor.execute('''
+	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS players (
 			player_id 			INTEGER,
 			server_id 			INTEGER,
@@ -26,46 +25,46 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 			PRIMARY KEY (player_id, server_id)
 			FOREIGN KEY (selected_demon_id) REFERENCES demons(id)
 		)
-	''')
+	""")
 
-	cursor.execute('''
+	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS servers (
 			server_id		INTEGER PRIMARY KEY,
 			player_count	INTEGER DEFAULT 1,
 			server_level	INTEGER DEFAULT 1
 		)
-	''')
+	""")
 
-	cursor.execute('''
+	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS player_demons (
 			player_id 		INTEGER,
 			server_id 		INTEGER,
 			demon_id		INTEGER,
 			stored_rank		INTEGER,
 			in_party		INTEGER CHECK (in_party IN (0, 1)),
-			on_loan			INTEGER 
-				DEFAULT 0 
+			on_loan			INTEGER
+				DEFAULT 0
 				CHECK (on_loan IN (0, 1)),
 			PRIMARY KEY (player_id, server_id, demon_id)
 			FOREIGN KEY (demon_id) REFERENCES demons (id)
 		)
-	''')
+	""")
 
-	cursor.execute('''
+	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS server_demons (
 			server_id		INTEGER,
 			player_id		INTEGER,
 			demon_id		INTEGER,
 			PRIMARY KEY (server_id, demon_id),
-			FOREIGN KEY (server_id) 
+			FOREIGN KEY (server_id)
 				REFERENCES servers (server_id),
-			-- A demon doesn't exist in server_demons in the usual sense, it's a reference pointing to player_demons. 
-			FOREIGN KEY (player_id, server_id, demon_id) 
+			-- A demon doesn't exist in server_demons in the usual sense, it's a reference pointing to player_demons.
+			FOREIGN KEY (player_id, server_id, demon_id)
 				REFERENCES player_demons (player_id, server_id, demon_id)
 		)
-	''')
+	""")
 
-	cursor.execute('''
+	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS player_gems (
 			player_id 		INTEGER,
 			server_id 		INTEGER,
@@ -74,9 +73,9 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 			quantity		INTEGER DEFAULT 0,
 			UNIQUE(player_id, server_id, gem_name)
 		)
-	''')
+	""")
 
-	cursor.execute('''
+	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS player_items (
 			player_id 		INTEGER,
 			server_id 		INTEGER,
@@ -84,11 +83,10 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 			quantity		INTEGER DEFAULT 0,
 			UNIQUE(player_id, server_id, item_id)
 		)
-	''')
+	""")
 
 	# Index for faster lookup of server's compendiums.
-	cursor.execute('''
+	cursor.execute("""
 		CREATE INDEX IF NOT EXISTS idx_server_demons_lookup
 			ON server_demons (server_id, player_id, demon_id)
-	''')
-
+	""")
