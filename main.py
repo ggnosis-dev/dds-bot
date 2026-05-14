6bf2e2ce133acdd3632cd23147683e1ec8f9257d
ggnosis-dev <ggnosis.var@pm.me>
2026-05-14 16:37:06 +1000

    Fixed main files for formatter

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 import asyncio
-import discord
 import os
+
+import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
 
 # Load the environment variables from the .env file.
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv("DISCORD_TOKEN")
 
 # Every permission needs to be enabled through intents.
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix=">", intents=intents)
+
 
 @bot.event
 async def on_ready():
-	print(f'{bot.user} has connected to Discord!')
+	print(f"{bot.user} has connected to Discord!")
 
 
 async def load_cogs():
 	async with bot:
-		await bot.load_extension('cogs.demons')
-		await bot.load_extension('cogs.check_handler')
-		await bot.load_extension('cogs.compendium')
-		await bot.load_extension('cogs.encounters')
-		await bot.load_extension('cogs.items')
-		await bot.load_extension('cogs.gems')
-		await bot.load_extension('cogs.party')
-		await bot.load_extension('cogs.server_compendium')
-		await bot.load_extension('cogs.shop_rags')
+		await bot.load_extension("cogs.demons")
+		await bot.load_extension("cogs.check_handler")
+		await bot.load_extension("cogs.compendium")
+		await bot.load_extension("cogs.encounters")
+		await bot.load_extension("cogs.items")
+		await bot.load_extension("cogs.gems")
+		await bot.load_extension("cogs.party")
+		await bot.load_extension("cogs.server_compendium")
+		await bot.load_extension("cogs.shop_rags")
 
 		if token != None:
 			await bot.start(token)
+
 
 asyncio.run(load_cogs())
--- shared_enums.py
+++ shared_enums.py
@@ -1,30 +1,36 @@
 from enum import Enum
 
+
 class Emotes(Enum):
-	ONE = '\u0031\ufe0f\u20e3'
-	TWO = '\u0032\ufe0f\u20e3'
-	THREE = '\u0033\ufe0f\u20e3'
-	ICON = '<:__:1486233309078884493>'
-	BLANK = '<:__:1486236397508628510>'
+	ONE = "\u0031\ufe0f\u20e3"
+	TWO = "\u0032\ufe0f\u20e3"
+	THREE = "\u0033\ufe0f\u20e3"
+	ICON = "<:__:1486233309078884493>"
+	BLANK = "<:__:1486236397508628510>"
+
 
 class Unicode(Enum):
-	FILLED_CIRCLE = '\u2b24'
-	UNFILLED_CIRCLE = '\u25ef'
+	FILLED_CIRCLE = "\u2b24"
+	UNFILLED_CIRCLE = "\u25ef"
 
-class Personality(Enum): 
+
+class Personality(Enum):
 	CHEERFUL = 1
 	SHY = 2
 	AGGRESSIVE = 3
+
 
 class DemonRegistration(Enum):
 	IN_PARTY = 1
 	IN_COMP = 2
 	UNREGISTERED = 3
 
+
 class ResponseType(Enum):
 	GOOD = 1
 	NEUTRAL = 2
 	BAD = 3
+
 
 class GemList(Enum):
 	AGATE = 0
