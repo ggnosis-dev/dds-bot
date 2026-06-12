bcb43de384952fe008c5ea69791c7bcc8ce56cbe
ggnosis-dev <ggnosis.var@pm.me>
2026-06-12 20:22:55 +1000

    Better explanations on build fusion chart script

--- scripts/build_fusion_chart_db.py
+++ scripts/build_fusion_chart_db.py
@@ -7,14 +7,11 @@ ensure_db_dir_exists()
 
 with open(FUSION_CSV_PATH, newline="") as f:
 	reader = csv.reader(f)
+
+	# Top row is a list of races (skip 1 due to blank cell).
 	headers = next(reader)
 	races = headers[1:]
 	rows = list(reader)
-
-	print(reader)
-	print(f"HEADERS: {headers}")
-	print(f"RACES: {races}")
-	print(f"ROWS: {rows}")
 
 	with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 		cursor = conn.cursor()
@@ -22,7 +19,7 @@ with open(FUSION_CSV_PATH, newline="") as f:
 		# Delete existing demon table in case changes to general structure.
 		cursor.execute("DROP TABLE IF EXISTS fusion_chart")
 
-		# Create demon table.
+		# Create fusion chart table.
 		cursor.execute("""
 			CREATE TABLE IF NOT EXISTS fusion_chart (
 				race_1 TEXT NOT NULL,
@@ -32,21 +29,23 @@ with open(FUSION_CSV_PATH, newline="") as f:
 			)
 		""")
 
+		# For every row, keeping track of the row index...
 		for i, row in enumerate(rows):
+			# e.g. ['Fairy', '', 'Element', '', '', '', 'Jaki', '', '']
+			# Index 0 will be the first ingredient in the fusion, the left-most entry/name in the chart.
 			race_1 = row[0]
 
-			for j, result in enumerate(row[1:], start=0):
-				print(f"INFO: i {i} j {j}")
-				# Header and column are in the same order so this would be doubling up.
-				# Skip empty cells too.
+			# For every potential "result" that the first race can turn into...
+			for j, result in enumerate(row[1:]):
+				# Header and column are in the same order so this would be doubling up. Skip empty cells too.
 				if j < i or not result:
-					print(f"Skipping. j < i: {j < i}. Result: {result}")
+					print(f"WARN: Skipping: {race_1} + {races[j]} = {result or 'N/A'}. i ({i}) | j ({j})")
 					continue
 
+				# j is the position of the result, so races[j] will be the race in the header.
 				race_2 = races[j]
-				# race_1, race_2 = sorted([race_1, race_2])
 
-				print(f"INFO: Putting in: {race_1} + {race_2}")
+				print(f"INFO: Putting in {race_1} + {race_2} = {result}")
 
 				cursor.execute(
 					"""
