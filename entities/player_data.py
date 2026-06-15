98a3e6dfa383558d7c5b3150148639f06585359b
ggnosis-dev <ggnosis.var@pm.me>
2026-06-15 10:47:15 +1000

    Player data dataclasses are here. Probably should move the demon ones

---
+++ entities/player_data.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PlayerData:
+	player_id: int
+	server_id: int
+	selected_demon_id: int
+	mag: int
+	daily_timer: int
+
+
+@dataclass
+class ServerCompendiumDemon:
+	"""Thin data class used for operations."""
+
+	player_id: int
+	server_id: int
+	demon_id: int
+	stored_rank: int
+
+
+@dataclass
+class DemonEntry:
+	"""For view/displaying player demons."""
+
+	demon_id: int
+	name: str
+	race: str
+	personality: str
+	rank: int | None
+	gem: str
+	# For player demon only.
+	in_party: bool | None = None
+	# For server demon only.
+	owner_id: int | None = None
+	owner: str | None = None
