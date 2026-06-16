618fe3239f24f571d6d29a78ebf48be994236ae3
ggnosis-dev <ggnosis.var@pm.me>
2026-06-16 14:49:14 +1000

    Moved encounter window check to utility and updated daily to instead provide MAG.

--- cogs/utility.py
+++ cogs/utility.py
@@ -3,18 +3,19 @@ import time
 from discord.ext import commands
 
 from helpers import checks
+from helpers.costs import daily_mag
 from helpers.views import MessageView
 from queries import currency_queries, player_queries
 
 # TODO: Remove this from encounters.py. Needs to be streamlined.
-DAILY_COOLDOWN = 43200
+DAILY_COOLDOWN = 43200 * 2
+WINDOW_HOURS = 3
 
 
 class Utility(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@checks.has_profile()
 	@commands.command(name="stuff", aliases=["st"], description="Check the stuff.")
 	async def stuff_check_command(self, ctx):
 		"""Command to view MAG, daily timer, all that jazz."""
@@ -22,8 +23,8 @@ class Utility(commands.Cog):
 		server_id = ctx.guild.id
 		player_data = await player_queries.get_player(player_id, server_id)
 		daily_string = "Daily is available!"
+		encounter_time_up = "Encounter is available!"
 		mag = 0
-		time_since = 0
 
 		if player_data:
 			mag = player_data.mag
@@ -32,15 +33,55 @@ class Utility(commands.Cog):
 			time_now = int(time.time())
 			time_since = time_now - player_data.daily_timer
 
-		# If still time, send a message with how long remaining.
-		if time_since < DAILY_COOLDOWN:
-			remaining = DAILY_COOLDOWN - time_since
-			hours, remainder = divmod(remaining, 3600)
-			minutes, seconds = divmod(remainder, 60)
+			# If still time, send a message with how long remaining.
+			if time_since < DAILY_COOLDOWN:
+				remaining = DAILY_COOLDOWN - time_since
+				hours, remainder = divmod(remaining, 3600)
+				minutes, seconds = divmod(remainder, 60)
 
-			daily_string = f"Daily available in **{hours}h**, **{minutes}m** and **{seconds}s**."
+				daily_string = f"Daily available in **{hours}h**, **{minutes}m** and **{seconds}s**."
 
-		view = MessageView(f"{daily_string}\n\nMAG: **{mag}**")
+			current_window = get_current_encounter_window(time_now)
+
+			# If encounter has already been made in this period, send a message with how long remaining.
+			if current_window < player_data.daily_timer:
+				window_seconds = WINDOW_HOURS * 3600
+				remaining = (current_window + window_seconds) - time_now
+				hours, remainder = divmod(remaining, 3600)
+				minutes, seconds = divmod(remainder, 60)
+
+				encounter_time_up = f"Encounter available in **{hours}h**, **{minutes}m** and **{seconds}s**."
+
+		view = MessageView(f"{encounter_time_up}\n\n{daily_string}\n\nMAG: **{mag}**")
+		await ctx.send(view=view)
+
+	@checks.has_profile()
+	@commands.command(name="daily", aliases=["d"], description="Get some daily MAG.")
+	async def daily_mag_command(self, ctx):
+		"""Command to view MAG, daily timer, all that jazz."""
+		player_id = ctx.author.id
+		server_id = ctx.guild.id
+		player_data = await player_queries.get_player(player_id, server_id)
+		daily_string = ""
+
+		if player_data:
+			time_now = int(time.time())
+			time_since = time_now - player_data.daily_timer
+
+			if time_since < DAILY_COOLDOWN:
+				remaining = DAILY_COOLDOWN - time_since
+				hours, remainder = divmod(remaining, 3600)
+				minutes, seconds = divmod(remainder, 60)
+
+				daily_string = f"Daily available in **{hours}h**, **{minutes}m** and **{seconds}s**."
+			else:
+				add_mag = daily_mag()
+				total_mag = player_data.mag + add_mag
+				currency_queries.update_mag(player_id, server_id, add_mag)
+				daily_string = f"You've found **+{add_mag}**! Your total is now **{total_mag}**."
+				await player_queries.set_daily_timer(player_id, server_id, time_now)
+
+		view = MessageView(f"{daily_string}")
 		await ctx.send(view=view)
 
 	@checks.is_developer()
@@ -54,6 +95,21 @@ class Utility(commands.Cog):
 
 		view = MessageView(f"Added {amount} MAG.\n\nTotal MAG: **{mag}**")
 		await ctx.send(view=view)
+
+
+def get_current_encounter_window(now: int) -> int:
+	"""Get the current encounter window in seconds. Man, this took me way too long."""
+	# Convert window hours to seconds.
+	window_seconds = WINDOW_HOURS * 3600
+
+	# How many times the window has elapsed since the beginning.
+	windows_elapsed = now // window_seconds
+
+	# Take the number of windows elapsed and multiply it by how long a window takes in seconds.
+	# We then know which window we're currently in.
+	this_window = windows_elapsed * window_seconds
+
+	return this_window
 
 
 async def setup(bot: commands.Bot) -> None:
--- helpers/costs.py
+++ helpers/costs.py
@@ -1,6 +1,13 @@
+import random
+
+
 def fusion_cost(demon_rank: int) -> int:
 	return max(int(demon_rank * 100), 1000)
 
 
 def summon_cost(demon_rank: int) -> int:
 	return max(int(demon_rank * 100), 1000)
+
+
+def daily_mag() -> int:
+	return random.randint(100, 500)
