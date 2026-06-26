eecdb9856fa9a0786895f3f6ff568658a872dc1f
ggnosis-dev <ggnosis.var@pm.me>
2026-06-26 20:55:47 +1000

    Added usage, hidden and require_var_positional to command_data. Aliases now optional

--- entities/command_data.py
+++ entities/command_data.py
@@ -4,8 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class CommandData:
 	name: str
-	aliases: list[str]
 	help: str
+	usage: str
+	aliases: list[str] | None = None
+	hidden: bool = False
+	require_var_positional: bool = False
 
 
 def command_kwargs(command_type: dict[str, CommandData], key: str):
@@ -14,6 +17,8 @@ def command_kwargs(command_type: dict[str, CommandData], key: str):
 		"name": data.name,
 		"aliases": data.aliases,
 		"help": data.help,
+		"usage": data.usage,
+		"hidden": data.hidden,
 	}
 
 
@@ -25,8 +30,8 @@ COMPENDIUM_COMMANDS = {
 			"View your Demonic Compendium, letting you see every demon you have ever recruited."
 			" You can view other player's Compendiums by tagging them,"
 			' and can view hidden columns by typing part of "gemstone" or "personality".'
-			"\ne.g. `>compendium {@player | gemstone | personality}"
 		),
+		usage=">compendium {opt: @player | gemstone | personality}",
 	),
 	"summon": CommandData(
 		name="summon",
@@ -34,8 +39,9 @@ COMPENDIUM_COMMANDS = {
 		help=(
 			"Summon a registered demon from your Demonic Compendium to your Party for a fee."
 			" Provide the demon's name after the command."
-			"\ne.g. `>summon {demon}`."
 		),
+		usage=">summon | >sum {demon}",
+		require_var_positional=True,
 	),
 }
 
@@ -43,14 +49,39 @@ DEMONS_COMMANDS = {
 	"select": CommandData(
 		name="select",
 		aliases=["sel"],
-		help=(
-			"Select a demon to lead your Party. The selected demon will hunt for gemstones while you use the server."
-			"\ne.g. `>select {demon}"
-		),
+		help="Select a demon to lead your Party. The selected demon will hunt for gemstones while you use the server.",
+		usage=">select | >sel {demon}",
 	),
 	"leader": CommandData(
 		name="leader",
 		aliases=["le"],
-		help="View the status of the demon leading your Party.\ne.g. `>leader",
+		help="View the status of the demon leading your Party.",
+		usage=">leader | >le",
+	),
+}
+
+ENCOUNTERS_COMMANDS = {
+	"encounter": CommandData(
+		name="encounter",
+		aliases=["e"],
+		help=(
+			"Starts an encounter with a demon."
+			" Its rank is determined at random using distribution up to the Server's Maximum Rank Capacity "
+			" with weight at your Party's Average Rank."
+		),
+		usage=">encounter | >e",
+	),
+	# TODO: Make deprecated.
+	"start": CommandData(
+		name="start",
+		help="Sets up the player to start playing with the DDS-BOT.",
+		usage=">start",
+	),
+	"test_encounter": CommandData(
+		name="test_encounter",
+		aliases=["te"],
+		help="Developer Only. Start a test encounter with a random demon.",
+		usage=">test_encounter | te {opt: demon}",
+		hidden=True,
 	),
 }
