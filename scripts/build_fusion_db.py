9d535b3ca9e5189f4ba10a7ce9968dbfab0280cd
ggnosis-dev <ggnosis.var@pm.me>
2026-07-01 12:22:40 +1000

    Added build special fusions script

---
+++ scripts/build_fusion_db.py
@@ -0,0 +1,87 @@
+import json
+import sqlite3
+
+from database_paths import PLAYERS_DB_PATH, SPECIAL_FUSION_JSON, ensure_db_dir_exists
+from queries.demon_queries import get_demon_id_by_name
+
+ensure_db_dir_exists()
+
+
+def load_fusion_recipes():
+	recipes = []
+	ingredients = []
+
+	with open(SPECIAL_FUSION_JSON) as f:
+		data = json.load(f)
+
+	for recipe_id, entry in enumerate(data, 1):
+		# Do fusion_recipes first.
+		name = entry["result_demon"]
+		result_id = get_demon_id_by_name(name)
+
+		print(f"INFO: Adding Special Fusion entry for: {name}")
+
+		new_recipe = (
+			recipe_id,
+			result_id,
+			entry["required_key"],
+		)
+
+		recipes.append(new_recipe)
+
+		# Do fusion ingredients next.
+		for i in entry["ingredients"]:
+			ingr_id = get_demon_id_by_name(i)
+
+			new_ingredient = (
+				recipe_id,
+				ingr_id,
+			)
+
+			ingredients.append(new_ingredient)
+
+	return recipes, ingredients
+
+
+with sqlite3.connect(PLAYERS_DB_PATH) as conn:
+	conn.execute("PRAGMA foreign_keys = ON")
+	cursor = conn.cursor()
+
+	# cursor.execute("DROP TABLE IF EXISTS sp_fusion_ingredients")
+	# cursor.execute("DROP TABLE IF EXISTS sp_fusion_recipes")
+
+	cursor.execute("""
+		CREATE TABLE IF NOT EXISTS sp_fusion_recipes (
+			id 					INTEGER PRIMARY KEY,
+			result_demon_id		INTEGER NOT NULL REFERENCES demons(id),
+			-- required_key should match LevelReward.value.
+			required_key 		TEXT NOT NULL
+		)
+	""")
+
+	cursor.execute("""
+		CREATE TABLE IF NOT EXISTS sp_fusion_ingredients (
+			recipe_id 	INTEGER NOT NULL REFERENCES sp_fusion_recipes(id),
+			demon_id	INTEGER NOT NULL REFERENCES demons(id),
+			PRIMARY KEY (recipe_id, demon_id)
+		)
+	""")
+
+	rec, ing = load_fusion_recipes()
+
+	# Insert fusion recipes data.
+	cursor.executemany(
+		"""
+			INSERT OR IGNORE INTO sp_fusion_recipes (id, result_demon_id, required_key)
+			VALUES (?, ?, ?)
+		""",
+		rec,
+	)
+
+	cursor.executemany(
+		"""
+			INSERT OR IGNORE INTO sp_fusion_ingredients (recipe_id, demon_id)
+			VALUES (?, ?)
+		""",
+		ing,
+	)
