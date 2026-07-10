19db3fe7b87eceeecbc89cd26b9c83f9a95d637d
ggnosis-dev <ggnosis.var@pm.me>
2026-07-10 14:19:32 +1000

    Added script to build talk tables

--- scripts/build_talk_db.py
+++ scripts/build_talk_db.py
@@ -2,10 +2,10 @@ import json
 import sqlite3
 
 from database_paths import PLAYERS_DB_PATH, TALK_JSON
-from queries.demon_queries import get_demon_id_by_name
+from shared_enums import Personality, ResponseType
 
 
-def load_fusion_recipes():
+def load_questions_answers():
 	all_questions = []
 	all_answers = []
 
@@ -27,18 +27,26 @@ def load_fusion_recipes():
 		)
 
 		all_questions.append(new_question)
-		print(all_questions)
 
-		# # Do fusion ingredients next.
-		# for i in entry["ingredients"]:
-		# 	ingr_id = get_demon_id_by_name(i)
+		# Do answers next.
+		for a in answers:
+			# Each answer has a label acting as the words the player says.
+			label = a["label"]
 
-		# 	new_ingredient = (
-		# 		recipe_id,
-		# 		ingr_id,
-		# 	)
+			for r in a["reactions"]:
+				pers = Personality[r["personality"]].value
+				typ = ResponseType[r["type"]].value
+				res = r["response"]
 
-		# 	ingredients.append(new_ingredient)
+				new_answer = (
+					talk_id,
+					pers,
+					typ,
+					label,
+					res,
+				)
+
+				all_answers.append(new_answer)
 
 	return all_questions, all_answers
 
@@ -47,44 +55,44 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 	conn.execute("PRAGMA foreign_keys = ON")
 	cursor = conn.cursor()
 
-	# cursor.execute("DROP TABLE IF EXISTS sp_fusion_ingredients")
-	# cursor.execute("DROP TABLE IF EXISTS sp_fusion_recipes")
+	cursor.execute("DROP TABLE IF EXISTS talk_questions")
+	cursor.execute("DROP TABLE IF EXISTS talk_answers")
 
-	# cursor.execute("""
-	# 	CREATE TABLE IF NOT EXISTS talk_questions (
-	# 		id 				INTEGER PRIMARY KEY,
-	# 		cheerful		TEXT NOT NULL,
-	# 		aggressive		TEXT NOT NULL,
-	# 		shy				TEXT NOT NULL,
-	# 	)
-	# """)
+	cursor.execute("""
+		CREATE TABLE IF NOT EXISTS talk_questions (
+			id 				INTEGER PRIMARY KEY,
+			cheerful		TEXT NOT NULL,
+			aggressive		TEXT NOT NULL,
+			shy				TEXT NOT NULL
+		)
+	""")
 
-	# cursor.execute("""
-	# 	CREATE TABLE IF NOT EXISTS talk_answers (
-	# 		talk_id 	INTEGER NOT NULL REFERENCES talk_questions(id),
-	# 		personality	INTEGER NOT NULL,
-	# 		reaction	INTEGER NOT NULL,
-	# 		label		TEXT NOT NULL,
-	# 		response	TEXT NOT NULL,
-	# 		PRIMARY KEY (talk_id, personality)
-	# 	)
-	# """)
+	cursor.execute("""
+		CREATE TABLE IF NOT EXISTS talk_answers (
+			talk_id 		INTEGER NOT NULL REFERENCES talk_questions(id),
+			personality		INTEGER NOT NULL,
+			response_type	INTEGER NOT NULL,
+			label			TEXT NOT NULL,
+			response		TEXT NOT NULL,
+			PRIMARY KEY (talk_id, personality)
+		)
+	""")
 
-	rec, ing = load_fusion_recipes()
+	q, a = load_questions_answers()
 
 	# Insert fusion recipes data.
-	# cursor.executemany(
-	# 	"""
-	# 		INSERT OR IGNORE INTO sp_fusion_recipes (id, result_demon_id, required_key)
-	# 		VALUES (?, ?, ?)
-	# 	""",
-	# 	rec,
-	# )
+	cursor.executemany(
+		"""
+			INSERT OR IGNORE INTO talk_questions (id, cheerful, aggressive, shy)
+			VALUES (?, ?, ?, ?)
+		""",
+		q,
+	)
 
-	# cursor.executemany(
-	# 	"""
-	# 		INSERT OR IGNORE INTO sp_fusion_ingredients (recipe_id, demon_id)
-	# 		VALUES (?, ?)
-	# 	""",
-	# 	ing,
-	# )
+	cursor.executemany(
+		"""
+			INSERT OR IGNORE INTO talk_answers (talk_id, personality, response_type, label, response)
+			VALUES (?, ?, ?, ?, ?)
+		""",
+		a,
+	)
