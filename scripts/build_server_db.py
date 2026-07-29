e2ea2e1c05414450a454279a943907d696301cc2
ggnosis-dev <ggnosis.var@pm.me>
2026-07-29 15:45:25 +1000

    Updated build_server_db script to remove faction columns and make things not null

--- scripts/build_server_db.py
+++ scripts/build_server_db.py
@@ -17,40 +17,32 @@ with sqlite3.connect(PLAYERS_DB_PATH) as conn:
 			server_level		INTEGER DEFAULT 1,
 			server_level_xp 	INTEGER DEFAULT 0,
 			-- Rank cap is upper bound of what a player can spawn.
-			rank_cap			INTEGER DEFAULT 5,
-
-			law_level			INTEGER DEFAULT 1,
-			law_rank_cap		INTEGER DEFAULT 5,
-			chaos_level			INTEGER DEFAULT 1,
-			chaos_rank_cap		INTEGER DEFAULT 5
+			rank_cap			INTEGER DEFAULT 3
 		)
 	""")
 
 	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS server_demons (
-			server_id		INTEGER,
-			player_id		INTEGER,
-			demon_id		INTEGER,
+			server_id		INTEGER NOT NULL,
+			player_id		INTEGER NOT NULL,
+			demon_id		INTEGER NOT NULL,
+			faction_id		INTEGER,
 			PRIMARY KEY 	(server_id, demon_id),
 			FOREIGN KEY 	(server_id)
 				REFERENCES 	servers (server_id),
 			-- A demon doesn't exist in server_demons in the usual sense, it's a reference pointing to player_demons.
 			FOREIGN KEY 	(player_id, server_id, demon_id)
-				REFERENCES 	player_demons (player_id, server_id, demon_id)
+				REFERENCES 	player_demons (player_id, server_id, demon_id),
+			FOREIGN KEY		(server_id, faction_id)
+				REFERENCES factions (server_id, faction_id)
 		)
 	""")
 
 	cursor.execute("""
 		CREATE TABLE IF NOT EXISTS server_unlocks (
 			-- Small relational table to keep track of what has been unlocked.
-			server_id		INTEGER,
-			unlock_key		TEXT,
+			server_id		INTEGER NOT NULL,
+			unlock_key		TEXT NOT NULL,
 			PRIMARY KEY (server_id, unlock_key)
 		)
-	""")
-
-	# Index for faster lookup of server's compendiums.
-	cursor.execute("""
-		CREATE INDEX IF NOT EXISTS idx_server_demons_lookup
-			ON server_demons (server_id, player_id, demon_id)
 	""")
