ad9cb61a8564395369a19425866fed0d24a0f9d6
ggnosis-dev <ggnosis.var@pm.me>
2026-09-02 13:01:56 +1000

    Small refactor for badges

--- cogs/profile.py
+++ cogs/profile.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 from entities.command_data import PROFILE_COMMANDS, command_kwargs
 from helpers import checks, gets
+from helpers.messages import ProfileMsg
 from queries import badge_queries
 
 
@@ -12,22 +13,12 @@ class Profile(commands.Cog):
 	@checks.has_profile()
 	@commands.command(**command_kwargs(PROFILE_COMMANDS, "badges"))
 	async def badges_command(self, ctx: commands.Context) -> None:
-		"""
-		Use an item on a demon.
-
-		Args:
-			input_str (str): String containing item name and optional demon name, separated by a semicolon delimiter.
-				"item_name; demon_name" or just "item_name" to use on selected demon.
-		"""
+		"""Show obtained badges."""
 
 		player_id, _server_id = gets.get_player_server_ids(ctx)
-		demon_badges = badge_queries.get_all_demon_badges(player_id)
-		show_badges = ""
-
-		for badge in demon_badges:
-			show_badges += f"<:{badge.name}:{badge.emote_id}>"
-
-		await ctx.send(show_badges)
+		badges = await badge_queries.get_all_demon_badges(player_id)
+		message = ProfileMsg.show_badges(badges) if len(badges) >= 0 else ProfileMsg.no_badges()
+		await ctx.send(message)
 
 
 async def setup(bot: commands.Bot) -> None:
